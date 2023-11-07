949377e15074c61c5fb92faf883f61aecace0384
David Andersson <lejonmanen@gmail.com>
2023-11-07 14:43:10 +0100

    forEach exempel

---
+++ listor.js
@@ -0,0 +1,22 @@
+// TODO: testa att hitta minsta värdet
+// samt att prova med negativa tal
+let numbers = [3, 7, 11, 18, 5]
+let max = 0
+for( let index=0; index < numbers.length; index++ ) {
+	console.log(numbers[index]);
+	if( numbers[index] > max ) {
+		max = numbers[index]
+	}
+}
+
+console.log('Största talet i listan är: ' + max);
+
+numbers.forEach(number => {
+	console.log('forEach: ' + number);
+})
+
+function forEach(lista, callback) {
+	for( let i=0; i<lista.length; i++ ) {
+		callback(lista[i])
+	}
+}
